98215af19e686d15b16c7d901885f28c17c10dad
Minh-Thong-06022002 <nguyenminhthong11b@gmail.com>
2023-07-02 14:53:42 +0700

    finish

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+// Sliders 
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +29,75 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Our works
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
+
+const tabActive = $(".tab-item.active");
+const line = $(".tabs .line");
+
+// tabs.forEach((tab, index) => {
+
+//   tab.onclick = function () {
+//     tabActive.classList.remove("active");
+
+//     line.style.left = this.offsetLeft + "px";
+//     line.style.width = this.offsetWidth + "px";
+
+//     this.classList.add("active");
+//   };
+// });
+panes.forEach((el) => {
+  el.classList.add("active2");
+})
+tabs.forEach((el) => {
+  el.addEventListener("click",openTabs);
+})
+function openTabs(el) {
+  var btn = el.currentTarget;
+  var electronic = btn.dataset.electronic;
+  
+  
+  line.style.left = this.offsetLeft + "px";
+  line.style.width = this.offsetWidth + "px";
+  panes.forEach((el) => {
+    el.classList.remove("active2");
+  })
+  var temp = document.querySelectorAll("." + electronic);
+  for (var i = 0; i < temp.length; i++) { 
+    temp[i].classList.add("active2");
+   }
+}
+
+
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+
+// const tabs = $$(".tab-item");
+// const panes = $$(".tab-pane");
+
+// const tabActive = $(".tab-item.active");
+// const line = $(".tabs .line");
+
+// // SonDN fixed - Active size wrong size on first load.
+// // Original post: https://www.facebook.com/groups/649972919142215/?multi_permalinks=1175881616551340
+// requestIdleCallback(function () {
+//   line.style.left = tabActive.offsetLeft + "px";
+//   line.style.width = tabActive.offsetWidth + "px";
+// });
+
+// tabs.forEach((tab, index) => {
+//   const pane = panes[index];
+
+//   tab.onclick = function () {
+//     $(".tab-item.active").classList.remove("active");
+//     $(".tab-pane.active").classList.remove("active");
+
+//     line.style.left = this.offsetLeft + "px";
+//     line.style.width = this.offsetWidth + "px";
+
+//     this.classList.add("active");
+//     pane.classList.add("active");
+//   };
+// });
